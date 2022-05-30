1a48b6c41833b21e118b8ab7d4f9f79ce08e8289
xiejian06033218 <xiejian06033218@hotmail.com>
2022-05-30 23:16:43 +1000

    task completed for submission

--- terraform_aws_infra/main.tf
+++ terraform_aws_infra/main.tf
@@ -76,7 +76,6 @@ module "web_host" {
   key_name               = "EC2_test"
   monitoring             = false
   vpc_security_group_ids = [aws_security_group.web_host-sg.id]
-  #subnet_id              = module.vpc.public_subnet_ids[0]
 
   tags = local.common_tags
 }
@@ -107,36 +106,3 @@ resource "aws_security_group" "web_host-sg" {
     ipv6_cidr_blocks = ["::/0"]
   }
 }
-
-# resource "null_resource" "web" {
-  
-#   triggers = {
-#     bastion_instance_id = "module.web_host.id"
-#     trigger_count = "4"
-#   }
-
-#   connection {
-#     type        = "ssh"
-#     user        = "ubuntu"
-#     host        = module.web_host.public_ip
-#     private_key = file("~/Documents/cloudkey/EC2_test.pem") //pass the private key file path
-#   }
-
-#   provisioner "remote-exec" {
-#     # install docker and awscli on the web host
-#     inline = [
-#       "sudo apt update",
-#       "sudo apt -y install apt-transport-https ca-certificates curl software-properties-common",
-#       "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -",
-#       "sudo add-apt-repository 'deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable'",
-#       "sudo apt update",
-#       "sudo apt -y install docker-ce",
-#       "sudo groupadd docker",
-#       "sudo usermod -aG docker $USER",
-#       "curl 'https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip' -o 'awscliv2.zip'",
-#       "sudo apt install unzip",
-#       "unzip awscliv2.zip",
-#       "sudo ./aws/install",
-#     ]
-#   }
-# }
